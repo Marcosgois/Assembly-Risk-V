eca1f174ef2189e7755fe322b085f9d2ca3778aa
Marcos Gois <marcosvagois@outlook.com>
2021-08-14 19:53:48 -0300

    Working Print and Input Vector

--- multBy2.asm
+++ multBy2.asm
@@ -1,8 +1,34 @@
-li  a7, 5           # service 5 is read an int from input console
-ecall
-li  t1, 2           # load 2 to get the double of the number in input
-mul a0, a0, t1       # load desired value into argument register a0, using pseudo-op
-li  a7, 1           # service 1 is print integer in a0
-ecall
-li  a7, 10           # Exits the program with code 0
-ecall
+
+.data
+
+	vector: 	.word   0x10040000		# endereco do vetor
+    
+.text
+    jal     loadAddr                    # jump to loadAddr
+    la      a1, vector                  # load address of "vector" in a1       
+    li      t1, 11                      # t1 = value for iterations   
+    sw      t1, 0(a1)                   # First position in vector is t1 ( 10 )
+    addi    a1, a1, 4                   # t0 = t1 + imm
+    
+    mv      t0, t1                      # load the first value for iterations
+
+ readInput:       
+    li      a7, 5                       # service 5 is read an int from input console
+    ecall                               # syscall
+    addi    t0, t0, -1                  # (--t0) -> t0 = t0 + (-1)imm
+    
+    
+printV:
+    jal     loadAddr                    # jump to loadAddr
+    la      a1, vector                  # load address of "vector" in a1    
+    li      t1, 2                       # load 2 to get the double of the number in input
+    mul     a0, a0, t1                  # load desired value into argument register a0, using pseudo-op
+    li      a7, 1                       # service 1 is print integer in a0
+    ecall                               # syscall
+exit:                                   
+    beq     t0, zero, readInput         # if t0 == 0 then jump to sortList
+    li      a7, 10                      # Exits the program with code 0
+    ecall
+loadAddr:
+    la      a1, vector                  # load address of "vector" in a1
+    jr      ra                          # jump to ra
---
+++ riscv4.asm
@@ -0,0 +1,64 @@
+
+.data
+
+	vector: 	.word   0x10040000		# endereco do vetor
+    
+.text
+	jal     loadAddr                    # jump to loadAddr
+	la      a1, vector                  # load address of "vector" in a1       
+	li      t1, 10                      # t1 = value for iterations   
+	sw      t1, 0(a1)                   # First position in vector is t1 ( 10 )
+	addi    a1, a1, 4                   # a1 = a1 + 4
+    
+	mv      t0, t1                      # load the first value for iterations
+	
+	li      a7, 5                       # service 5 is read an int from input console
+	ecall                               # syscall
+	sw	    a0, (a1)			        # load input value to vector
+	addi    a1, a1, 4                   # a1 = a1 + 4
+	addi    t0, t0, -1                 	# (--t0) -> t0 = t0 + (-1)imm
+	addi	t2, t2, 4			        # [index]
+
+ readInput:       
+ 	ble     t0,zero, printV			    # jump if t0 (iterador) <= 0
+	li      a7, 5                       # service 5 is read an int from input console
+	ecall                               # syscall
+	addi    t0, t0, -1                 	# (--t0) -> t0 = t0 + (-1)imm
+	lw	    t1, -4(a1)			        # load the value of a1[index-1] in t1 to compare
+
+	ble 	a0, t1, sort			    # if t2(a1[i]) <= a0 ? sort : continue
+	sw	    a0, (a1)			        # load input value to vector
+	addi    a1, a1, 4                   # a1 = a1 + 4
+	addi	t2, t2, 4			        # [index]
+	j	    readInput			        # return to readInput
+
+sort:
+	addi	t2, t2, -4			        # [index-4]
+	sw	    a0, -4(a1)			        # [index-4]
+	sw	    t1, (a1)			
+	bge  	t2, zero, sort			    # if t2 >= 0 ? sort : continue
+	j	    readInput
+	
+    
+printV:
+	jal     loadAddr                    # jump to loadAddr
+	lw	    t0, (a1)			        # load vector size
+	addi    a1, a1, 4                   # a1 = a1 + 4
+pV:
+	lw	    a0, (a1)			        # int to print
+	li      a7, 1                       # service 1 is print integer in a0
+	ecall                               # syscall
+	
+	addi    t0, t0, -1                 	# (--t0) -> t0 = t0 + (-1)imm
+	ble 	t0, zero, exit			    # if t2(a1[i]) <= 0 ? sort : continue
+	addi    a1, a1, 4                   # a1 = a1 + 4
+	j	    pV
+
+	
+exit:           
+	li      a7, 10                      # Exits the program with code 0
+	ecall
+	
+loadAddr:
+	la      a1, vector                  # load address of "vector" in a1
+	jr      ra                          # jump to ra
